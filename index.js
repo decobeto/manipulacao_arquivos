37c2931c762fdf76b9bac2c31b895ea487d40e1f
André Roberto <32197501+decobeto@users.noreply.github.com>
2019-05-28 00:19:35 -0300

    Adicionada função que vai savar itens dentro da tabela
    Adicionada a função para salvar itens, tambem foram feitas algumas alterações no html e css, removendo a tag style e movendo o css para dentro do arquivo css

--- index.js
+++ index.js
@@ -46,41 +46,14 @@ function clearLocalStorage() {
 
 // Gravando novos registros
 
-var campos = [
-    document.querySelector('#key'),
-    document.querySelector('#name'),
-    document.querySelector('#description')
-]
-
-console.log(campos)
-
-var tbody = document.querySelector('table tbody')
-
-document.querySelector('.form').addEventListener('submit', function(event) {
-    
-    // cancelando a submissão do formulário
-    event.preventDefault()
-
-    var tr = document.createElement('tr')
-
-    campos.forEach(function(campo) {
-        // cria uma td sem informações
-        var td = document.createElement('td')
-
-        // atribui o valor do campo à td
-        td.textContent = campo.value
-
-        //adiciona a td na tr
-        tr.appendChild(td)
-    })
-
-    // limpa o campo da data
-    campos[0].value = 1
-    // limpa o campo da quantidade
-    campos[1].value = ''
-    // limpa o campo do valor
-    campos[2].value = ''
-})
+function salvandoNoArmazenamento(){
+  event.preventDefault();
+  let ultimaKey = localStorage.length;
+  let keyDoItemSalvo = ultimaKey + 1;
+  let campos = { "Nome": document.querySelector('#nome').value, "Informação": document.querySelector('#informacao').value};
+  window.localStorage.setItem(keyDoItemSalvo, JSON.stringify(campos));
+  console.log(campos)
+}
 
 // console.log(JSON.parse(window.localStorage.getItem(item)))
 //         var registro = JSON.parse(window.localStorage.getItem(item))
