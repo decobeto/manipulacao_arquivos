65f47fa930c4f915a5e11734ff210f400f36fe54
Raul dos Santos Moraes <rauldosS.moraes@gmail.com>
2019-05-27 22:32:30 -0300

    Commit
    commitando

--- index.js
+++ index.js
@@ -40,6 +40,44 @@ function clearLocalStorage() {
     window.localStorage.clear()
 }
 
+// Gravando novos registros
+
+var campos = [
+    document.querySelector('#key'),
+    document.querySelector('#name'),
+    document.querySelector('#description')
+]
+
+console.log(campos)
+
+var tbody = document.querySelector('table tbody')
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+    
+    // cancelando a submissão do formulário
+    event.preventDefault()
+
+    var tr = document.createElement('tr')
+
+    campos.forEach(function(campo) {
+        // cria uma td sem informações
+        var td = document.createElement('td')
+
+        // atribui o valor do campo à td
+        td.textContent = campo.value
+
+        //adiciona a td na tr
+        tr.appendChild(td)
+    })
+
+    // limpa o campo da data
+    campos[0].value = 1
+    // limpa o campo da quantidade
+    campos[1].value = ''
+    // limpa o campo do valor
+    campos[2].value = ''
+})
+
 // console.log(JSON.parse(window.localStorage.getItem(item)))
 //         var registro = JSON.parse(window.localStorage.getItem(item))
 //         console.log(registro.Nome)
