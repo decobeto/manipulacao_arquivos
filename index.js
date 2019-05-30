201df3f79c345af87af2556d9100461bb9104d96
Raul dos Santos Moraes <rauldosS.moraes@gmail.com>
2019-05-29 22:37:40 -0300

    Commit pra conseguir dar Pull

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function insertTable(value, registro) {
   if(registro != null) {
     // imprime todo o registro na tabela
     row.innerHTML = "<td>" + value + "</td> <td>" + registro.Nome + "</td> <td>" + registro.Informação + "</td>"
-    + "<a onclick=\"editItem(" + value + ")\" class=\"waves-effect waves-light btn-small tooltipped scrollLink effect\" href=\"#editar\" data-position=\"right\" data-tooltip=\"Editar registro\" style=\"margin-top: 0.6rem;margin-right: 1rem;\"><i class=\"material-icons\">edit</i></a>"
+    + "<a onclick=\"editItem(" + value + ")\" class=\"waves-effect waves-light btn-small tooltipped scrollLink\" href=\"#editar\" data-position=\"right\" data-tooltip=\"Editar registro\" style=\"margin-top: 0.6rem;margin-right: 1rem;\"><i class=\"material-icons\">edit</i></a>"
     + "<a onclick=\"removeItem(" + value + ")\" class=\"waves-effect waves-light btn-small tooltipped red lighten-1\" data-position=\"right\" data-tooltip=\"Remover registro\" style=\"margin-top: 0.6rem;margin-right: 1rem;\"><i class=\"material-icons\">delete</i></a>"
     countRegistro++
   }
